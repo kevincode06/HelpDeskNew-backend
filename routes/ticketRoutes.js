207d8beb5b86b793d463dbed5f74a74c4e9cc53d
“Kevincode6” <maloukevin6@gmail.com>
2025-07-08 12:45:30 +0200

    fixed ticketRoute

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -5,13 +5,21 @@ const {
     getTickets,
     getTicket,
     updateTicket,
+    addReply,
+    getAllTickets,
+    updateTicketStatus
 } = require('../controllers/ticketController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect, authorizeAdmin } = require('../middleware/authMiddleware');
 
+// User routes
 router.route('/').post(protect, createTicket).get(protect, getTickets);
 router.route('/:id').get(protect, getTicket).put(protect, updateTicket);
+router.route('/:id/replies').post(protect, addReply);
 
 
+// admin routes
+router.route('/admin').get(protect, authorizeAdmin, getAllTickets);
+router.route('/admin/:id/status').get(protect, authorizeAdmin, updateTicketStatus);
 module.exports = router;
 
 
