e750c79e0a3b534ed421b3f880e2781acd9c2519
“Kevincode6” <maloukevin6@gmail.com>
2025-07-08 09:46:51 +0200

    authMiddleware.js

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+const User = require('./models/userModel');
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // Get Token from header
+            token = req,headers.authorization.split(' ')[1];
+            // verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            // get user from token
+            req.user = await User.findById(decoded.id).select('-password');
+            next();
+        } catch (error) {
+            console.error(error);
+            res.status(401);
+            throw new Error('Not authorized, token failed');
+        }
+    }
+
+    if (!token) {
+        res.status(401);
+        throw new Error('Not authorized, no token');
+    }
+});
+
+
+const authorizeAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(403);
+        throw new Error('Not authorizes as an admin');
+    }
+};
+
+module.exports = { protect, authorizeAdmin}; 
